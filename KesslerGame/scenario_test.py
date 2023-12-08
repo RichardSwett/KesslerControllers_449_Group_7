7686425156c81814bcd5ceb871ebcd6ffb6ffe1d
kavish52 <kavishra@ualberta.ca>
2023-12-08 13:55:28 -0700

    Add deafult value for chromosome

--- KesslerGame/scenario_test.py
+++ KesslerGame/scenario_test.py
@@ -9,6 +9,7 @@ from src.kesslergame import Scenario, KesslerGame, GraphicsType
 from FuzzyController import GroupSevenController
 from EasyGA.structure import Chromosome
 from Controller_Defs import read_list_from_file
+import os
 
 
 my_test_scenario = Scenario(name='Test Scenario',
@@ -30,7 +31,21 @@ game = KesslerGame(settings=game_settings)  # Use this to visualize the game sce
 # game = TrainerEnvironment(settings=game_settings)  # Use this for max-speed, no-graphics simulation
 
 pre = time.perf_counter()
-score, perf_data = game.run(scenario=my_test_scenario, controllers = [GroupSevenController(chromosome = Chromosome(read_list_from_file("best_chromosome.pk1")))])
+
+if os.path.exists("best_chromosome.pk1"):
+
+    chromosome = Chromosome(read_list_from_file("best_chromosome.pk1"))
+
+else: 
+
+    chromosome = Chromosome([[[0.004909619761891005, 0.024494051314874955, 0.056946675102348465], 
+                   [35, 95, 120], [59.419856828504486, 98.7598202838615, 159.2490580996916], 
+                   [43.313031350255784, 52.58580234991643, 278.8065261700964, 493.1566041420365, 683.5075146580989], 
+                   [58.243503838056945, 101.23232459042296, 142.76971633525383], 
+                   [186.99693276638138], 
+                   [2.9522636948690675, 5.805887299076666, 8.211396101189177]]])
+
+score, perf_data = game.run(scenario=my_test_scenario, controllers = [GroupSevenController(chromosome = chromosome)])
 
 print('Scenario eval time: '+str(time.perf_counter()-pre))
 print(score.stop_reason)
